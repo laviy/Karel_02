a88655f6e1fca5d4ccc8a0afb7fd5d82b21563a5
laviy <yonatan_lavi@asl.org>
2014-10-30 14:43:23 +0000

    Lab02

--- Lab02.java
+++ Lab02.java
@@ -1,9 +1,9 @@
 /**
 *
-* Description of the program goes here  // provide a brief description
+* Escape the Maze!!!! 
 *
-* @author <Your Name here>  // replace <...> with your name
-* @version <date/of/completion> // replace <...> with the date
+* @author <Yoni Lavi>  // replace <...> with your name
+* @version <30/10/2014> // replace <...> with the date
 */
 
 import edu.fcps.karel2.Display;
@@ -12,6 +12,49 @@ import edu.fcps.karel2.Robot;
 public class Lab02 {
 
      public static void main(String[] args) {
-          // TODO Your code goes here
+          Display.openWorld("maps/maze.map");
+     Display.setSize(8, 8);
+     Display.setSpeed(5);
+     Athlete bob = new Athlete();
+     bob.putBeeper();
+     bob.move();
+     bob.putBeeper();
+     bob.turnRight();
+     bob.move();
+     bob.putBeeper();
+     bob.turnRight();
+     bob.move();
+     bob.putBeeper();
+     bob.turnLeft();
+     bob.move();
+     bob.putBeeper();
+     bob.turnLeft();
+     bob.move();
+     bob.putBeeper();
+     bob.turnRight();
+     bob.move();
+     bob.putBeeper();
+     bob.move();
+     bob.putBeeper();
+     bob.turnRight();
+     bob.move();
+     bob.putBeeper();
+     bob.turnLeft();
+     bob.move();
+     bob.putBeeper();
+     bob.turnLeft();
+     bob.move();
+     bob.putBeeper();
+     bob.move();
+     bob.putBeeper();
+     bob.move();
+     bob.putBeeper();
+     bob.move();
+     bob.putBeeper();
+     bob.turnRight();
+     bob.move();
+     bob.putBeeper();
+     bob.move();
+     
      }
 }
